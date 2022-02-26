e9cd7537cb4d462ddcdf35e462511bea30a0af2b
Matthias Keller <matthias.keller1@gmx.de>
2022-02-26 03:00:39 +0100

    inital commit

---
+++ HIDBootloader.cs
@@ -0,0 +1,509 @@
+﻿/*
+ * SPDX-FileCopyrightText: © 2021 Matthias Keller <mkeller_service@gmx.de>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Lib_mphidflashsharp
+{
+   public class Bootloader
+    {
+        private byte[] UsbWriteBuffer;
+        private byte[] UsbReadBuffer;
+        private HIDAPIInterface.USBHIDDevice usbdev = null;
+
+        private HIDAPIInterface.DeviceScanner usbdevScan = null;
+
+        public HIDAPIInterface.USBHIDDevice Usbdev
+        {
+            get { return usbdev; }
+        }
+
+        private QueryResponse queryRes = new QueryResponse();
+        private ExtendedQueryResponse exQueryRes = new ExtendedQueryResponse();
+        private IntelHex.IntelHexFile write_hexfile;
+        private byte[] readbuf = null;
+
+        public byte[] ReadBuffer
+        {
+            get { return readbuf; }
+        }
+
+        public bool Connected { get => connected;  }
+
+        private Boolean connected = false;
+        public bool verbose = false;
+        private UInt16 VID = 0x04D8;
+        private UInt16 PID = 0x003C;
+        public Bootloader()
+        {
+            UsbWriteBuffer = new byte[64];
+            UsbReadBuffer = new byte[64];
+            usbdevScan = new HIDAPIInterface.DeviceScanner(VID, PID, 200);
+            Connect();
+
+        }
+        public Bootloader(UInt16 vid, UInt16 pid)
+        {
+            UsbWriteBuffer = new byte[64];
+            UsbReadBuffer = new byte[64];
+            VID = vid;
+            PID = pid;
+            usbdevScan = new HIDAPIInterface.DeviceScanner(VID, PID, 200);
+            Connect();
+        }
+
+        public bool ExportReadBin(string outpath)
+        {
+            bool ret = false;
+            FileStream fs = null;
+            BinaryWriter bw = null;
+            if (readbuf == null)
+            {
+                Console.WriteLine("Nothing read. No binary export.");
+                return false;
+            }
+            Console.WriteLine("Export flash content...");
+            try
+            {
+                fs = new FileStream(outpath, FileMode.Create, FileAccess.Write);
+                bw = new BinaryWriter(fs);
+                bw.Write(readbuf, 0, readbuf.Length);
+                Console.WriteLine("Flash content exported to {0}", outpath);
+                ret = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                if (bw != null)
+                    bw.Close();
+                if (fs != null)
+                    fs.Close();
+            }
+            return ret;
+        }
+
+        public bool ReadFlash(string outpath)
+        {
+            bool ret = ReadFlash();
+            ret = ExportReadBin(outpath);
+            return ret;
+        }
+
+        public byte[] ReadData(UInt32 address, int count)
+        {
+            if (!usbdev.IsOpen || !connected)
+                return null;
+
+            byte[] readbuf = new byte[count];
+            for (int i = 0; i < readbuf.Length; i++)
+                readbuf[i] = 0xff;
+            GenericPacket tx_pkt = new GenericPacket();
+            GenericPacket rx_pkt = new GenericPacket();
+
+            tx_pkt.Command = BootCommands.GET_DATA;
+
+            tx_pkt.Address = address;
+            tx_pkt.Size = (byte)count;
+            Array.Clear(UsbWriteBuffer, 0, UsbReadBuffer.Length);
+            Console.Write("Reading {0} bytes from address 0x{1:X5}: ", tx_pkt.Size, address);
+            UsbWriteBuffer = tx_pkt.Packet;
+            int writtenbytes = usbdev.Write(UsbWriteBuffer);
+            if (writtenbytes > 0)
+            {
+                Array.Clear(UsbReadBuffer, 0, UsbReadBuffer.Length);
+                int readbyte = usbdev.Read(UsbReadBuffer, 200);
+                if (readbyte >= 64)
+                {
+
+                    rx_pkt.Packet = UsbReadBuffer;
+                    if (verbose)
+                        Console.WriteLine("OK. {0} bytes read.", rx_pkt.Size);
+                    Array.Copy(rx_pkt.Data, 0, readbuf, 0, rx_pkt.Size);
+
+                    address += rx_pkt.Size;
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Error while reading {0}. USB write error.", address);
+               
+            }
+            return readbuf;
+
+        }
+        public bool ReadFlash()
+        {
+            bool ret=false;
+            if (!usbdev.IsOpen || !connected)
+                return false;
+            readbuf = new byte[0x1FFFe];
+            for (int i = 0; i < readbuf.Length; i++)
+                readbuf[i] = 0xff;
+            GenericPacket tx_pkt = new GenericPacket();
+            GenericPacket rx_pkt = new GenericPacket();
+            tx_pkt.Command = BootCommands.GET_DATA;
+            UInt32 address = queryRes.ProgramMemStart;
+            uint end_address=queryRes.ProgramMemStart + queryRes.ProgramMemLength;
+
+
+            Console.WriteLine("\r\nStart Reading Flash!\r\n");
+            long pkt = 0;
+            while (address < end_address)
+            {
+
+                tx_pkt.Address = address;
+                //if((end_address - address) > 58)
+                    tx_pkt.Size = 58;
+                //else
+                //    tx_pkt.Size = (byte)(end_address - address);
+
+                Array.Clear(UsbWriteBuffer, 0, UsbReadBuffer.Length);
+                UsbWriteBuffer = tx_pkt.Packet;
+                if(verbose)
+                    Console.Write("Reading {0} bytes from address 0x{1:X5}: ",tx_pkt.Size, address);
+                int writtenbytes = usbdev.Write(UsbWriteBuffer);
+                if (writtenbytes > 0)
+                {
+                    Array.Clear(UsbReadBuffer, 0, UsbReadBuffer.Length);
+                    int readbyte = usbdev.Read(UsbReadBuffer, 200);
+                    if (readbyte >= 64)
+                    {
+
+                        rx_pkt.Packet = UsbReadBuffer;
+                        if (verbose)
+                            Console.WriteLine("OK. {0} bytes read.", rx_pkt.Size);
+                        Array.Copy(rx_pkt.Data, 0, readbuf, address, rx_pkt.Size);
+
+                        address += rx_pkt.Size;
+                    }
+                    pkt++;
+
+                }
+                else
+                {
+                    Console.WriteLine("Error while reading {0}. USB write error.", address);
+                    return false;
+                }
+
+            }
+            Console.Write("Successfully read from address 0x{0:X5} to  address 0x{1:X5}", queryRes.ProgramMemStart, address);
+
+            return ret;
+        }
+        public bool WriteFlashFromBin(string binfile)
+        {
+            bool ret = false;
+            FileStream fs = null;
+            byte[] bindata = null;
+            try
+            {
+                fs = new FileStream(binfile, FileMode.Open, FileAccess.Read);
+                bindata = new byte[fs.Length];
+                fs.Read(bindata, 0, (int)fs.Length);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                if (fs != null)
+                    fs.Close();
+            }
+
+            Console.WriteLine("Start writing flash with binfile {0}. Binary size: {1}", binfile, bindata.Length);
+            ret = WriteFlash(bindata);
+
+            return ret;
+
+        }
+        public bool WriteFlash(byte[] writedata)
+        {
+            if (!usbdev.IsOpen || !connected)
+                return false;
+
+            Console.WriteLine("Start writing flash with binary size: {0}", writedata.Length);
+            //actual write
+            double progress = 0.0;
+            int writtentotalbytes = 0;
+            UInt32 address = queryRes.ProgramMemStart;
+            while (address < (queryRes.ProgramMemStart + queryRes.ProgramMemLength))
+            {
+                int writtenbytes = writeBlock(address, writedata);
+                if (writtenbytes > 0)
+                    address += (uint)writtenbytes;
+                else
+                {
+                    Console.WriteLine("Error while writing {0}", address);
+                    Console.WriteLine("Cancel and erasing device again");
+                    EraseDevice();
+                    return false;
+                }
+                writtentotalbytes += writtenbytes;
+                progress = 100 * (double)writtentotalbytes / (double)writedata.Length;
+                int progint = (int)(progress * 100);
+                if (progint % 500 == 0 && progint > 0)
+                    Console.WriteLine("Progess: {0:0.0}%", progress);
+            }
+            if (queryRes.bootloader_V101OrNewer)
+            {
+                Console.WriteLine("Bootloader with V101 or newer. Signing flash.");
+                Array.Clear(UsbWriteBuffer, 0, UsbReadBuffer.Length);
+                GenericPacket pkt = new GenericPacket();
+                pkt.Command = BootCommands.SIGN_FLASH;
+                UsbWriteBuffer = pkt.Packet;
+                Console.WriteLine("SIGN_FLASH");
+                int transferbytes = usbdev.Write(UsbWriteBuffer);
+
+            }
+            else
+                Console.WriteLine("Old Bootloader. Flash content not signed.");
+            return true;
+
+        }
+
+        public bool WriteFlashHexFile(string hexpath)
+        {
+            if (!usbdev.IsOpen || !connected)
+                return false;
+            write_hexfile = new IntelHex.IntelHexFile();
+            FileStream fs = null;
+            StreamReader sr = null;
+            try
+            {
+                fs = new FileStream(hexpath, FileMode.Open, FileAccess.Read);
+                sr = new StreamReader(fs);
+                write_hexfile.LoadFromHexFile(sr);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                if (sr != null)
+                    sr.Close();
+                if (fs != null)
+                    fs.Close();
+            }
+
+            if (!write_hexfile.FileValid)
+            {
+                Console.WriteLine("File {0} contains errors. Abort.", hexpath);
+                return false;
+            }
+
+            Console.WriteLine("Start writing flash with File {0}. Binary size: {1}", hexpath, write_hexfile.BinarySize);
+            bool ret = WriteFlash(write_hexfile.Binary);
+            return ret;
+
+        }
+
+
+        /// <summary>
+        /// Writes one (USB HID) packet (max 58 bytes) to the flash block.
+        /// </summary>
+        /// <param name="address">Startaddress of the flashblock. Have to be an even address</param>
+        /// <param name="data">Bytearray with the content to write. </param>
+        /// <returns></returns>
+        private int writeBlock(UInt32 address, byte[] data)
+        {
+            if (!usbdev.IsOpen || !connected)
+                return -1;
+            Array.Clear(UsbWriteBuffer, 0, UsbReadBuffer.Length);
+
+            GenericPacket pkt = new GenericPacket();
+            pkt.Command = BootCommands.PROGRAM_DEVICE;
+            if ((address & 0x1) != 0)
+            {
+                Console.WriteLine("Error on write: address {0} not even", address);
+                return -1;
+            }
+            pkt.Address = address;
+
+            if ((data.Length - address) >= 58)
+                pkt.Size = 58;
+            else
+            {
+                pkt.Command = BootCommands.PROGRAM_COMPLETE;
+                pkt.Size = (byte)(data.Length - address);
+            }
+            Array.Copy(data, address, pkt.Data, 0, pkt.Size);
+
+            UsbWriteBuffer = pkt.Packet;
+            if(verbose)
+                Console.WriteLine("Write at address 0x{0}. Len {1}", address.ToString("X6"), pkt.Size);
+            int writtenbytes = usbdev.Write(UsbWriteBuffer);
+            if (writtenbytes >= 64)
+                return pkt.Size;
+            else
+                return -1;
+
+
+        }
+
+        public bool ResetDevice()
+        {
+            if (!usbdev.IsOpen || !connected)
+                return false;
+            Array.Clear(UsbReadBuffer, 0, UsbReadBuffer.Length);
+            Array.Clear(UsbWriteBuffer, 0, UsbReadBuffer.Length);
+
+            UsbWriteBuffer[0] = (byte)BootCommands.RESET_DEVICE;
+            Console.WriteLine("Send Reset Command");
+            int writtenbytes = usbdev.Write(UsbWriteBuffer);
+            Thread.Sleep(200);
+            if (writtenbytes > 0)
+                return true;
+            else
+                return false;
+        }
+
+        public bool EraseDevice()
+        {
+            if (!usbdev.IsOpen || !connected)
+                return false;
+            Array.Clear(UsbReadBuffer, 0, UsbReadBuffer.Length);
+            Array.Clear(UsbWriteBuffer, 0, UsbReadBuffer.Length);
+            UsbWriteBuffer[0] = (byte)BootCommands.ERASE_DEVICE;
+            Console.WriteLine("Start Erase");
+            int writtenbytes = usbdev.Write(UsbWriteBuffer);
+            Thread.Sleep(200);
+            Array.Clear(UsbWriteBuffer, 0, UsbReadBuffer.Length);
+
+            bool queryOK = false;
+            QueryTx();
+            Stopwatch timeout = new Stopwatch();
+            timeout.Start();
+            do
+            {
+                queryOK = QueryRx(200);
+                if (timeout.Elapsed.Seconds > 40)
+                {
+                    Console.WriteLine("Timeout on erase.");
+                    return false;
+                }
+                Thread.Sleep(100);
+                Console.Write(".");
+            }
+            while (!queryOK);
+            Console.WriteLine("Erase completed");
+            return true;
+        }
+
+        public bool QueryExtended()
+        {
+            if (!usbdev.IsOpen)
+                return false;
+            //extended query
+            if (queryRes.bootloader_V101OrNewer)
+            {
+                Array.Clear(UsbReadBuffer, 0, UsbReadBuffer.Length);
+                Array.Clear(UsbWriteBuffer, 0, UsbWriteBuffer.Length);
+                UsbWriteBuffer[0] = (byte)BootCommands.QUERY_EXTENDED_INFO;
+                int writtenbytes = usbdev.Write(UsbWriteBuffer);
+                int readbyte = usbdev.Read(UsbReadBuffer, 1000);
+                if (readbyte >= 64)
+                {
+                    exQueryRes.ParseArray(UsbReadBuffer);
+                    exQueryRes.Print();
+                    return true;
+                }
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
+        public bool Query()
+        {
+            bool ret = false;
+            ret = QueryTx();
+            ret = QueryRx(200);
+            if (ret)
+                queryRes.Print();
+            return ret;
+        }
+        private bool QueryRx(int timeout)
+        {
+            Array.Clear(UsbReadBuffer, 0, UsbReadBuffer.Length);
+            int readbyte = usbdev.Read(UsbReadBuffer, timeout);
+            if (readbyte >= 64)
+            {
+                queryRes.ParseArray(UsbReadBuffer);
+                return true;
+            }
+            else
+                return false;
+        }
+        private bool QueryTx()
+        {
+            Array.Clear(UsbWriteBuffer, 0, UsbWriteBuffer.Length);
+            UsbWriteBuffer[0] = (byte)BootCommands.QUERY_DEVICE;
+            int writtenbytes = usbdev.Write(UsbWriteBuffer);
+            if (writtenbytes >= 64)
+                return true;
+            else
+                return false;
+        }
+        public void Disconnect()
+        {
+            if (usbdev != null)
+            {
+                if (usbdev.IsOpen)
+                {
+                    usbdev.Dispose();
+                }
+            }
+            connected = false;
+        }
+
+
+        public bool Connect()
+        {
+            
+            if (!usbdevScan.ScanOnce())
+            {
+                connected = false;
+                if (usbdev != null)
+                    usbdev.Dispose();
+                return false;
+            }
+            else
+            {
+                if (!connected)
+                {
+                    if (usbdev != null)
+                        usbdev.Dispose();
+                    usbdev = new HIDAPIInterface.USBHIDDevice(VID, PID, null, false, 64);
+                    if (!usbdev.IsOpen)
+                        return false;
+                }
+            }
+            queryRes = new QueryResponse();
+            exQueryRes = new ExtendedQueryResponse();
+            bool con = Query();
+            QueryExtended();
+            if (queryRes.Command == (byte)BootCommands.QUERY_DEVICE)
+                connected = true;
+            else
+                connected = false;
+            return connected;
+        }
+    }
+
+}
---
+++ HIDBootloaderDefs.cs
@@ -0,0 +1,226 @@
+﻿/*
+ * SPDX-FileCopyrightText: © 2021 Matthias Keller <mkeller_service@gmx.de>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Lib_mphidflashsharp
+{
+    enum BootCommands : byte
+    {
+        QUERY_DEVICE = 0x02, //Command that the host uses to learn about the device (what regions can be programmed, and what type of memory is the region)
+        UNLOCK_CONFIG = 0x03, //Note, this command is used for both locking and unlocking the config bits (see the "//Unlock Configs Command Definitions" below)
+        ERASE_DEVICE = 0x04, //Host sends this command to start an erase operation. Firmware controls which pages should be erased.
+        PROGRAM_DEVICE = 0x05, //If host is going to send a full RequestDataBlockSize to be programmed, it uses this command.
+        PROGRAM_COMPLETE = 0x06, //If host send less than a RequestDataBlockSize to be programmed, or if it wished to program whatever was left in the buffer, it uses this command.
+        GET_DATA = 0x07, //The host sends this command in order to read out memory from the device. Used during verify (and read/export hex operations)
+        RESET_DEVICE = 0x08, //Resets the microcontroller, so it can update the config bits (if they were programmed, and so as to leave the bootloader (and potentially go back into the main application)
+        SIGN_FLASH = 0x09, //The host PC application should send this command after the verify operation has completed successfully. If checksums are used instead of a true verify (due to ALLOW_GET_DATA_COMMAND being commented), then the host PC application should send SIGN_FLASH command after is has verified the checksums are as exected. The firmware will then program the SIGNATURE_WORD into flash at the SIGNATURE_ADDRESS.
+        QUERY_EXTENDED_INFO = 0x0C //Used by host PC app to get additional info about the device, beyond the basic NVM layout provided by the query device command
+    }
+
+    enum BootResponseType : byte
+    {
+        //Query Device Response "Types" 
+        MEMORY_REGION_PROGRAM_MEM = 0x01, //When the host sends a QUERY_DEVICE command, need to respond by populating a list of valid memory regions that exist in the device (and should be programmed)
+        MEMORY_REGION_EEDATA = 0x02,
+        MEMORY_REGION_CONFIG = 0x03,
+        MEMORY_REGION_USERID = 0x04,
+        MEMORY_REGION_END = 0xFF, //Sort of serves as a "null terminator" like number, which denotes the end of the memory region list has been reached.
+        BOOTLOADER_V1_01_OR_NEWER_FLAG = 0xA5 //Tacked on in the VersionFlag byte, to indicate when using newer version of bootloader with extended query info available
+    }
+
+    class GenericPacket
+    {
+        public BootCommands Command;
+        public UInt32 Address;
+        public byte Size;
+        public byte[] Data;
+        public GenericPacket()
+        {
+            usbPacket = new byte[64];
+            Data = new byte[58];
+        }
+
+        private byte[] usbPacket;
+        public byte[] Packet
+        {
+            get
+            {
+                usbPacket[0] = (byte)Command;
+                usbPacket[1] = (byte)(Address & 0x000000FFU);
+                usbPacket[2] = (byte)((Address & 0x0000FF00U) >> 8);
+                usbPacket[3] = (byte)((Address & 0x00FF0000U) >> 16);
+                usbPacket[4] = (byte)((Address & 0xFF000000U) >> 24);
+                usbPacket[5] = Size;
+                Array.Copy(Data, 0, usbPacket, 6, 58);
+                return usbPacket;
+            }
+            set
+            {
+                usbPacket = value;
+                Command = (BootCommands)usbPacket[0];
+                Address = BitConverter.ToUInt32(usbPacket, 1);
+                Size = usbPacket[5];
+                Array.Copy(usbPacket, 6, Data, 0, 58);
+            }
+        }
+    }
+
+    class ExtendedQueryResponse
+    {
+        public byte Command;
+        public UInt16 BootloaderVersion;
+        public UInt16 ApplicationVersion;
+        public UInt32 SignatureAddress;
+        public UInt16 SignatureValue;
+        public UInt32 ErasePageSize;
+        public byte Config1LMask;
+        public byte Config1HMask;
+        public byte Config2LMask;
+        public byte Config2HMask;
+        public byte Config3LMask;
+        public byte Config3HMask;
+        public byte Config4LMask;
+        public byte Config4HMask;
+        public byte Config5LMask;
+        public byte Config5HMask;
+        public byte Config6LMask;
+        public byte Config6HMask;
+        public byte Config7LMask;
+        public byte Config7HMask;
+        public void Print()
+        {
+            Console.WriteLine("BootloaderVersion:  0x{0} V{1}.{2}", BootloaderVersion.ToString("X4"), (BootloaderVersion >> 8).ToString(), (BootloaderVersion & 0xff).ToString());
+            Console.WriteLine("ApplicationVersion: 0x{0} V{1}.{2}", ApplicationVersion.ToString("X4"), (ApplicationVersion >> 8).ToString(), (ApplicationVersion & 0xff).ToString());
+            Console.WriteLine("SignatureAddress:   0x{0}", SignatureAddress.ToString("X8"));
+            Console.WriteLine("SignatureValue:     0x{0}", SignatureValue.ToString("X4"));
+        }
+        public void ParseArray(byte[] buf)
+        {
+            if (buf.Length < 64)
+                return;
+            int i = 0;
+            Command = buf[i++];
+            BootloaderVersion = BitConverter.ToUInt16(buf, i); i += 2;
+            ApplicationVersion = BitConverter.ToUInt16(buf, i); i += 2;
+            SignatureAddress = BitConverter.ToUInt32(buf, i); i += 4;
+            SignatureValue = BitConverter.ToUInt16(buf, i); i += 2;
+            ErasePageSize = BitConverter.ToUInt32(buf, i); i += 4;
+            Config1LMask = buf[i++];
+            Config1HMask = buf[i++];
+            Config2LMask = buf[i++];
+            Config2HMask = buf[i++];
+            Config3LMask = buf[i++];
+            Config3HMask = buf[i++];
+            Config4LMask = buf[i++];
+            Config4HMask = buf[i++];
+            Config5LMask = buf[i++];
+            Config5HMask = buf[i++];
+            Config6LMask = buf[i++];
+            Config6HMask = buf[i++];
+            Config7LMask = buf[i++];
+            Config7HMask = buf[i++];
+
+        }
+
+    }
+    class QueryResponse
+    {
+        public byte Command;
+        public byte PacketDataFieldSize;
+        public byte BytesPerAddress;
+        public UInt32 ProgramMemStart;
+        public UInt32 ProgramMemLength;
+        public byte Type1;
+        public UInt32 Address1;
+        public UInt32 Length1;
+        public byte Type2;
+        public UInt32 Address2;
+        public UInt32 Length2;
+        public byte Type3;
+        public UInt32 Address3;
+        public UInt32 Length3;
+        public byte Type4;
+        public UInt32 Address4;
+        public UInt32 Length4;
+        public byte Type5;
+        public UInt32 Address5;
+        public UInt32 Length5;
+        public byte Type6;
+        public UInt32 Address6;
+        public UInt32 Length6;
+        public byte VersionFlag;
+        public byte[] ExtraPadBytes;
+        public bool bootloader_V101OrNewer = false;
+
+        public void Print(bool verbose = false)
+        {
+            if (verbose)
+            {
+                Console.WriteLine("PacketDataFieldSize:                   0x{0}", PacketDataFieldSize.ToString("X2"));
+                Console.WriteLine("BytesPerAddress:                       0x{0}", BytesPerAddress.ToString("X2"));
+                Console.WriteLine("Type1 (MEMORY_REGION_PROGRAM_MEM):     0x{0}", Type1.ToString("X2"));
+                Console.WriteLine("Address1 (PROGRAM_MEM_START_ADDRESS):  0x{0}", Address1.ToString("X8"));
+                Console.WriteLine("Length1 (Size of program memory area): 0x{0}", Length1.ToString("X8"));
+                Console.WriteLine("Type2 (MEMORY_REGION_CONFIG):          0x{0}", Type2.ToString("X2"));
+                Console.WriteLine("Address2 (CONFIG_WORDS_START_ADDRESS): 0x{0}", Address2.ToString("X8"));
+                Console.WriteLine("Length2 (CONFIG_WORDS_SECTION_LENGTH): 0x{0}", Length2.ToString("X8"));
+                Console.WriteLine("Type3 (MEMORY_REGION_END):             0x{0}", Type2.ToString("X2"));
+            }
+            if (bootloader_V101OrNewer)
+                Console.WriteLine("Bootloader Version V1.1 or newer");
+            else
+                Console.WriteLine("Bootloader old Version V1.0");
+        }
+        public void ParseArray(byte[] buf)
+        {
+            if (buf.Length < 64)
+                return;
+            int i = 0;
+
+            Command = buf[i++];
+            PacketDataFieldSize = buf[i++];
+            BytesPerAddress = buf[i++];
+            Type1 = buf[i++];
+            Address1 = BitConverter.ToUInt32(buf, i); i += 4;
+            ProgramMemStart = Address1;
+            Length1 = BitConverter.ToUInt32(buf, i); i += 4;
+            ProgramMemLength = Length1;
+
+            Type2 = buf[i++];
+            Address2 = BitConverter.ToUInt32(buf, i); i += 4;
+            Length2 = BitConverter.ToUInt32(buf, i); i += 4;
+            Type3 = buf[i++];
+            Address3 = BitConverter.ToUInt32(buf, i); i += 4;
+            Length3 = BitConverter.ToUInt32(buf, i); i += 4;
+            Type4 = buf[i++];
+            Address4 = BitConverter.ToUInt32(buf, i); i += 4;
+            Length4 = BitConverter.ToUInt32(buf, i); i += 4;
+            Type5 = buf[i++];
+            Address5 = BitConverter.ToUInt32(buf, i); i += 4;
+            Length5 = BitConverter.ToUInt32(buf, i); i += 4;
+            Type6 = buf[i++];
+            Address6 = BitConverter.ToUInt32(buf, i); i += 4;
+            Length6 = BitConverter.ToUInt32(buf, i); i += 4;
+
+            VersionFlag = buf[i++];
+            if (VersionFlag == 0xA5)
+                bootloader_V101OrNewer = true;
+            else
+                bootloader_V101OrNewer = false;
+            ExtraPadBytes = new byte[6];
+            Array.Copy(buf, i, ExtraPadBytes, 0, 6);
+        }
+    }
+
+
+}
